1e470325d3956b0087e52d341ddd8b33453d20ac
Mark Ivan Basto <markivan.developer@outlook.com>
2023-02-08 11:52:11 +0800

    add tests

---
+++ BibleBooks/BibleBooks.Tests/KeyTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Globalization;
+
+namespace BibleBooks.Tests
+{
+    public class KeyTests
+    {
+        [Fact]
+        public void KeyForName()
+        {
+            foreach (var book in Enum.GetValues<BibleBook>())
+            {
+                var name = BibleBooksHelper.GetName(book);
+                Assert.Equal(BibleBooksHelper.GetKeyForName(name), book);
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInfoGenerator))]
+        public void KeyForNameWithCulture(CultureInfo culture)
+        {
+            foreach (var book in Enum.GetValues<BibleBook>())
+            {
+                var name = BibleBooksHelper.GetName(book, culture);
+                Assert.Equal(BibleBooksHelper.GetKeyForName(name, culture), book);
+            }
+        }
+
+        [Fact]
+        public void KeyForOsisCode()
+        {
+            foreach (var book in Enum.GetValues<BibleBook>())
+            {
+                var osis = BibleBooksHelper.GetOsisCode(book);
+                Assert.Equal(BibleBooksHelper.GetKeyForOsisCode(osis), book);
+            }
+        }
+
+        [Fact]
+        public void KeyForParatextCode()
+        {
+            foreach (var book in Enum.GetValues<BibleBook>())
+            {
+                var paratext = BibleBooksHelper.GetParatextCode(book);
+                Assert.Equal(BibleBooksHelper.GetKeyForParatextCode(paratext), book);
+            }
+        }
+
+        [Fact]
+        public void KeyForStandardAbbreviation()
+        {
+            foreach (var book in Enum.GetValues<BibleBook>())
+            {
+                var standard = BibleBooksHelper.GetStandardAbbreviation(book);
+                Assert.Equal(BibleBooksHelper.GetKeyForStandardAbbreviation(standard), book);
+            }
+        }
+
+        [Fact]
+        public void KeyForThompsonAbbreviation()
+        {
+            foreach (var book in Enum.GetValues<BibleBook>())
+            {
+                var thompson = BibleBooksHelper.GetThompsonAbbreviation(book);
+                Assert.Equal(BibleBooksHelper.GetKeyForThompsonAbbreviation(thompson), book);
+            }
+        }
+    }
+}
